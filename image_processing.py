8a3eda44468a31f0206a321e3c02e272086a898f
Gavin <152362542+ggboyles@users.noreply.github.com>
2025-11-20 18:10:24 -0600

    Resolve merge conflict in image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -5,10 +5,18 @@ from PIL import Image
 def convert_to_greyscale():
 
 	#Load image using PIL
-	img = Image.open('test.jpg')
+	img = Image.open('xy/light_000.png')
 
 	#Convert img to NumPy array
 	img_array = np.array(img)
 
 	#Grayscale conversion formula: Y= 0.299*R + 0.587*G + 0.114*B
 	gray_img = np.dot (img_array[..., :3], [0.299, 0.587, 0.114])
+
+	brightest_pixel = gray_img.max()
+
+	coords = np.unravel_index(np.argmax(gray_img), gray_img.shape)
+	print("brightest coordinates:", coords)
+
+
+convert_to_greyscale()
